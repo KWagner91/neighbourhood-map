f74d71bdb59a103ff3b830292d16be59f3ecefde
Kerstin Wagner <kerstin-wagner@gmx.at>
2018-08-22 17:10:31 +0200

    feat: Demo Info Window displaying marker name

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -1,9 +1,33 @@
 // https://www.fullstackreact.com/articles/how-to-write-a-google-maps-react-component/#the-map-container-component
+// https://www.npmjs.com/package/google-maps-react
 
 import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
  
 export class GoogleMap extends Component {
+	
+	state = {
+		showingInfoWindow: false,
+		activeMarker: {},
+		selectedPlace: {},
+  };
+	
+	 onMarkerClick = (props, marker, e) =>
+		this.setState({
+		  selectedPlace: props,
+		  activeMarker: marker,
+		  showingInfoWindow: true
+    });
+ 
+	onMapClicked = (props) => {
+		if (this.state.showingInfoWindow) {
+		  this.setState({
+			showingInfoWindow: false,
+			activeMarker: null
+      })
+    }
+  };
+	
   render() {
 	  
 	  const style = {
@@ -21,13 +45,19 @@ export class GoogleMap extends Component {
         lng: 11.5820
           }}
           zoom={14}
-          onClick={this.onMapClicked}
+         onClick={this.onMapClicked}
 	>
       
-     
- 
-        <Marker onClick={this.onMarkerClick}
+      <Marker onClick={this.onMarkerClick}
                 name={'Current location'} />
+ 
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
  
       </Map>
     );
