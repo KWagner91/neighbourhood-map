4192d4e96b7f409978d272e05b548ac48ce53ba6
Kerstin Wagner <kerstin-wagner@gmx.at>
2018-08-23 16:46:45 +0200

    feat: Prop Types added to Component

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -3,21 +3,28 @@
 
 import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import PropTypes from "prop-types";
  
 export class GoogleMap extends Component {
+	
+	static propTypes = {
+	  onMapClicked: PropTypes.func.isRequired,
+	  onMarkerClick: PropTypes.func.isRequired
+}
 	
 	state = {
 		showingInfoWindow: false,
 		activeMarker: {},
-		selectedPlace: {},
+		selectedPlace: {}
   };
 	
-	 onMarkerClick = (props, marker, e) =>
+	 onMarkerClick = (props, marker, e) => {
 		this.setState({
 		  selectedPlace: props,
 		  activeMarker: marker,
 		  showingInfoWindow: true
     });
+}
  
 	onMapClicked = (props) => {
 		if (this.state.showingInfoWindow) {
@@ -27,6 +34,11 @@ export class GoogleMap extends Component {
       })
     }
   };
+  
+  fetchPlaces(mapProps, map) {
+  const {google} = mapProps;
+  const service = new google.maps.places.PlacesService(map);
+}
 	
   render() {
 	  
@@ -37,9 +49,11 @@ export class GoogleMap extends Component {
 	  
     return (
       <Map 
-      google={this.props.google} 
-      style={style}
-      className={'map'}
+		google={this.props.google}
+		onReady= {this.props.fetchPlaces}
+		style={style}
+      
+     
       initialCenter={{
 		lat: 48.1351,
         lng: 11.5820
@@ -47,19 +61,25 @@ export class GoogleMap extends Component {
           zoom={14}
          onClick={this.onMapClicked}
 	>
+
+
       
       <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
+                name={'Current location'}
+                position={this.props.google.maps.LatLng()} />
+                
  
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}>
             <div>
               <h1>{this.state.selectedPlace.name}</h1>
+              <p>{this.state.selectedPlace.position}</p>
             </div>
         </InfoWindow>
  
       </Map>
+      
     );
   }
 }
