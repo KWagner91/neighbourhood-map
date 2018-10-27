0786aa67781d8906d492e518482dca54b8bec2c0
Kerstin Wagner <kerstin-wagner@gmx.at>
2018-10-27 14:51:03 +0200

    search box added without functionality

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,7 +1,10 @@
+// SOURCES
+// https://dev.to/sage911/how-to-write-a-search-component-with-suggestions-in-react-d20
+
 import React, {Component} from 'react'
 import ReactDom from 'react-dom'
 import PropTypes from 'prop-types';
-import GoogleMaps from './GoogleMaps'
+
 
 class SideMenu extends Component {
 	// Prop types
@@ -10,14 +13,33 @@ class SideMenu extends Component {
 };
 
 
+	state = {
+		query: '',
+	}
+
+
+	 handleInputChange = () => {
+	   this.setState({
+		 query: this.search.value
+	   })
+	 }
 
 	render() {
+		const places = this.props.items;
+		
 		return (
 		 <div id="right-panel">
 			  <h3>Search For Coffee</h3>
-			  <ul id="places"></ul>
-			  <button id="more">More results</button>
-			  
+			   <form>
+				   <input
+					 placeholder="Search for..."
+					 ref={input => this.search = input}
+					 onChange={this.handleInputChange}
+				   />
+				   <p>{this.state.query}</p>
+			 </form>
+				  
+				   
 			</div>
 		)
 		}
